eb3ef4f666c03f724e6cd2d8e1525d33e21d2404
Kevin La <la.kevin151@gmail.com>
2023-12-25 14:42:30 -0800

    Initial commit

--- bot.py
+++ bot.py
@@ -3,19 +3,36 @@ from cogs.destiny2_cogs import DestinyCogs
 import discord
 import json
 
-with open("credentials.json", "r") as file:
-    token = json.load(file)["discord_oauth"]
+def get_auth_token() -> str:
+    try:
+        with open("credentials.json", "r") as file:
+            return json.load(file)["discord_oauth"]
+    except:
+        print("=== 'credentials.json' file is unavailable. Please check directory and try again.")
 
-intents = discord.Intents.default()
-intents.message_content = True
-intents.typing = False
+def initialize_bot(token:str):
+    intents = discord.Intents.default()
+    intents.message_content = True
 
-#client = discord.Client(intents=intents)
-client = discord.ext.commands.Bot(command_prefix="/", intents=intents)
+    client = discord.ext.commands.Bot(command_prefix="!", intents=intents)
+ 
+    @client.event
+    async def on_ready():
+        print("=== Addings Cogs")
+        await client.add_cog(DestinyCogs(client))
 
-@client.event
-async def on_ready():
-    print("=== Addings Cogs")
-    await client.add_cog(DestinyCogs(client))
+        try:
+            synced = await client.tree.sync()
+            print(f"Synced {len(synced)} commands!")
+        except Exception as e:
+            print(e)
+    
+    client.run(token)
 
-client.run(token)
+if __name__ == '__main__':
+    discord_token = get_auth_token()
+
+    if discord_token:
+        initialize_bot(discord_token)
+    else:
+        print("=== Bot.py is unable to retrieve token. ")
--- cogs/destiny2_cogs.py
+++ cogs/destiny2_cogs.py
@@ -1,11 +1,23 @@
 from utils.mysql_interface import MySql_Interface
+import discord
+from typing import Optional
+from discord import app_commands
 from discord.ext import commands
 
 class DestinyCogs(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        print("Hi!")
     
-    @commands.command(name="PeePee")
-    async def hello_command(self,ctx):
-        await ctx.send("I am a cog machine")
+    @app_commands.command(name="schedule_activity", description="To schedule an activity amongst guardians.")
+    @app_commands.describe(activity="The description of activity you want to schedule.")
+    @app_commands.choices(activity=[
+        app_commands.Choice(name="Raid", value=1),
+        app_commands.Choice(name="Dungeon", value=2)
+    ])
+    async def schedule(self, interaction: discord.Interaction, activity:Optional[app_commands.Choice[int]]):
+        print(f"Activity Chosen: {activity}")
+        await interaction.response.send_message(f" {interaction.user.id}", ephemeral=True)
+    
+    # @app_commands.command(name="eeek", description="parade")
+    # async def hello2(self, interaction: discord.Interaction):
+    #     await interaction.response.send_message(f" {interaction.user.id}", ephemeral=True)
